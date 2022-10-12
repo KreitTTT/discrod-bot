9e6044d3bd975e9fa2c50051e8d9c7d8d14612aa
KreitTTT <46221903+KreitTTT@users.noreply.github.com>
2022-10-12 15:37:51 +0300

    Update 12/10/22

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from config import settings
+import random
 import json
 import requests
 
@@ -29,8 +30,9 @@ async def on_command_error(ctx, error):
 @bot.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def clear(ctx, amount: int=10):
+    author = ctx.message.author
     await ctx.channel.purge(limit=amount)
-    await ctx.send(f"Ты удалил {amount} сообщений...")
+    await ctx.send(f"{author} удалил {amount} сообщений...")
 
 @bot.command()
 async def hello(ctx):
@@ -122,7 +124,7 @@ async def help(ctx):
     emb.add_field(name='rimage', value='Доступные случайные картинки у бота')
     emb.add_field(name='dev', value='Что бы узнать кто мой создатель и хозяин')
     emb.add_field(name='cit', value='Рандомная цитата')
-
+    emb.add_field(name='roulette', value='Проверь свою удачу в русской рулетке')
     await ctx.send(embed = emb)
 
 @bot.command()
@@ -317,5 +319,118 @@ async def sketchheads(ctx):
     link = json.loads(response.content)
 
     await ctx.send(f"https://discord.com/invite/{link['code']}")
+
+@bot.command()
+async def roulette(ctx):
+    await ctx.send("Делаю выстрел :gun: ")
+    author = ctx.message.author
+    if random.randint(0, 6) == 4:
+        await ctx.send(f'{author.mention} погиб :skull_crossbones: ')
+    else:
+        await ctx.send(f'{author.mention} выжил, тебе сегодня везет!')
+
+@bot.command()
+async def vsy(ctx):
+    await ctx.send('https://youtu.be/7FjjnWfcJ-k')
+
+@bot.command()
+async def minesweeper(ctx, columns=None, rows=None, bombs=None):
+        if columns is None or rows is None and bombs is None:
+            if columns is not None or rows is not None or bombs is not None:
+                await ctx.send("error")
+                return
+            else:
+                columns = random.randint(3, 11)
+                rows = random.randint(3, 11)
+                bombs = columns * rows - 1
+                bombs = bombs / 2.5
+                bombs = round(random.randint(5, round(bombs)))
+        try:
+            columns = int(columns)
+            rows = int(rows)
+            bombs = int(bombs)
+        except ValueError:
+            await ctx.send("error")
+            return
+        if columns > 13 or rows > 13:
+            await ctx.send('Ограничение для столбцов и строк составляет 13 из-за дискорда..')
+            return
+        if columns < 1 or rows < 1 or bombs < 1:
+            await ctx.send('Введеные числа не могут быть нулевыми или отрицательными...')
+            return
+        if bombs + 1 > columns * rows:
+            await ctx.send(
+                ':boom:**BOOM**, у вас больше бомб, чем ячеек в сетке, или вы попытались сделать все ячейки бомбами!')
+            return
+
+        grid = [[0 for num in range(columns)] for num in range(rows)]
+
+        loop_count = 0
+        while loop_count < bombs:
+            x = random.randint(0, columns - 1)
+            y = random.randint(0, rows - 1)
+            if grid[y][x] == 0:
+                grid[y][x] = 'B'
+                loop_count = loop_count + 1
+            if grid[y][x] == 'B':
+                pass
+
+        pos_x = 0
+        pos_y = 0
+        while pos_x * pos_y < columns * rows and pos_y < rows:
+            adj_sum = 0
+            for (adj_y, adj_x) in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1)]:
+                try:
+                    if grid[adj_y + pos_y][adj_x + pos_x] == 'B' and adj_y + pos_y > -1 and adj_x + pos_x > -1:
+                        adj_sum = adj_sum + 1
+                except Exception as error:
+                    pass
+            if grid[pos_y][pos_x] != 'B':
+                grid[pos_y][pos_x] = adj_sum
+            if pos_x == columns - 1:
+                pos_x = 0
+                pos_y = pos_y + 1
+            else:
+                pos_x = pos_x + 1
+
+        string_builder = []
+        for the_rows in grid:
+            string_builder.append(''.join(map(str, the_rows)))
+        string_builder = '\n'.join(string_builder)
+        # Replaces the numbers and B for the respective emotes and spoiler tags
+        string_builder = string_builder.replace('0', '||:zero:||')
+        string_builder = string_builder.replace('1', '||:one:||')
+        string_builder = string_builder.replace('2', '||:two:||')
+        string_builder = string_builder.replace('3', '||:three:||')
+        string_builder = string_builder.replace('4', '||:four:||')
+        string_builder = string_builder.replace('5', '||:five:||')
+        string_builder = string_builder.replace('6', '||:six:||')
+        string_builder = string_builder.replace('7', '||:seven:||')
+        string_builder = string_builder.replace('8', '||:eight:||')
+        final = string_builder.replace('B', '||:bomb:||')
+
+        percentage = columns * rows
+        percentage = bombs / percentage
+        percentage = 100 * percentage
+        percentage = round(percentage, 2)
+
+        embed = discord.Embed(title='\U0001F642 Сапер \U0001F635', color=0xC0C0C0)
+        embed.add_field(name='Столбцы:', value=columns, inline=True)
+        embed.add_field(name='Ряды:', value=rows, inline=True)
+        embed.add_field(name='Всего ячеек:', value=columns * rows, inline=True)
+        embed.add_field(name='\U0001F4A3 Счет:', value=bombs, inline=True)
+        embed.add_field(name='\U0001F4A3 Доля:', value=f'{percentage}%', inline=True)
+        embed.add_field(name='Запрос от:', value=ctx.author.display_name, inline=True)
+        await ctx.send(content=f'\U0000FEFF\n{final}', embed=embed)
+
+@bot.command()
+async def d6(ctx):
+    await ctx.send(f':game_die: У тебе выпало {random.randint(1,6)}')
+@bot.command()
+async def d12(ctx):
+    await ctx.send(f':game_die: У тебе выпало {random.randint(1,12)}')
+@bot.command()
+async def d100(ctx):
+    await ctx.send(f':game_die: У тебе выпало {random.randint(1,100)}')
 
 bot.run(settings['token'])
